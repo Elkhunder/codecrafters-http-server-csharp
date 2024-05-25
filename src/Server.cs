e05845ccfda88506a30d9edccaf320bb78f63b47
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-05-24 23:42:22 -0400

    stage 3

--- src/Server.cs
+++ src/Server.cs
@@ -4,37 +4,138 @@ using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
-
-//Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-//
-server.Start();
-Byte[] bytes = new Byte[256];
-String data == null;
-
-// Enter listening loop
-while (true)
+TcpListener? server = null;
+try
 {
-    Console.WriteLine("Waiting for connection...");
-    using var socket = server.AcceptSocket();
-    Console.WriteLine("Connected!");
+    //Uncomment this block to pass the first stage
+    var routes = new List<string> { "/" };
 
-    data = null;
+    var ipEndpoint = new IPEndPoint(IPAddress.Loopback, 4221);
+    server = new TcpListener(ipEndpoint);
     
-    // Get stream object for reading and writing
-    var stream = new NetworkStream(socket);
+    server.Start();
+    var bytes = new byte[256];
 
-    int i;
-    
-    // loop to receive all data
-    while ((i = stream.Read(bytes, 0, bytes.Length))!=0)
+    // Enter listening loop
+    while (true)
     {
-        data = Encoding.ASCII.GetString(bytes, 0, i);
-        Console.WriteLine("Received: {0}", data);
+        Console.WriteLine("Waiting for connection...");
+        using var socket = server.AcceptSocket();
+        Console.WriteLine("Connected!");
 
-        data = data.ToUpper();
+        // Get stream object for reading and writing
+        var stream = new NetworkStream(socket);
 
-        byte[] msg = Encoding.ASCII.GetBytes(data);
+        int i;
+
+        // loop to receive all data
+        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+        {
+            var httpRequest = Encoding.ASCII.GetString(bytes, 0, i);
+            Console.WriteLine("Received: {0}", httpRequest);
+            var request = HandleRequest(httpRequest);
+
+            var hostEndpoint = new IPEndPoint(request.Headers.Address, request.Headers.Port);
+            if (hostEndpoint.Equals(ipEndpoint) && routes.Contains(request.Resource))
+            {
+                
+                var returnBuffer = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");    
+                stream.Write(returnBuffer, 0, returnBuffer.Length);
+            }
+            else
+            {
+                var returnBuffer = Encoding.ASCII.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+                stream.Write(returnBuffer, 0, returnBuffer.Length);
+            }
+        }
     }
 }
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+catch (SocketException e)
+{
+    Console.WriteLine("SocketException: {0}", e);
+}
+finally
+{
+    server?.Stop();
+}
+Console.WriteLine("\nHitEnter to continue");
+Console.Read();
+return;
+
+Request HandleRequest(string httpRequest)
+{
+    var request = new Request();
+    var requestLines = httpRequest.Split(["\n", "\r\n"], StringSplitOptions.RemoveEmptyEntries);
+    var requestHeader = request.Headers;
+    
+    foreach (var line in requestLines)
+    {
+        // Check if the line contains a header, indicated by a colon
+        if (line.Contains(':'))
+        {
+            // Split the line into header name and value
+            var headerParts = line.Split([":"], 2, StringSplitOptions.None);
+            switch (headerParts[0])
+            {
+                case "Host":
+                    var hostParts = headerParts[1].Split(':');
+                    var hostname = hostParts[0].Trim();
+
+                    if (hostname.Equals("localhost", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        requestHeader.Address = IPAddress.Loopback;
+                    }
+                    else
+                    {
+                        if (IPAddress.TryParse(hostname, out var address))
+                        {
+                            requestHeader.Address = address;
+                        }
+                    }
+                    Console.WriteLine($"Added IP Address to Request Header: {requestHeader.Address}");
+                    
+                    if (int.TryParse(hostParts[1], out var port))
+                    {
+                        requestHeader.Port = port;
+                        Console.WriteLine($"Added IP Port to Request Header: {requestHeader.Port}");
+                    }
+                    break;
+                case "User-Agent":
+                    requestHeader.UserAgent = headerParts[1];
+                    Console.WriteLine($"Added User Agent to Request Header");
+                    Console.WriteLine($"User Agent: {requestHeader.UserAgent}");
+                    break;
+            }
+            
+        }
+        else
+        {
+            // Assuming this is the request line, split it by spaces
+            var requestParts = line.Split(' ');
+            if (requestParts.Length < 3) continue;
+            request.Method = requestParts[0];
+            request.Resource = requestParts[1];
+            request.ProtocolVersion = requestParts[2];
+                
+            Console.WriteLine($"Method: {request.Method}, Resources: {request.Resource}, Protocol Version: {request.ProtocolVersion}");
+        }
+    }
+    
+    return request;
+}
+
+public record Request
+{
+    public string Method { get; set; } = string.Empty;
+    public string Resource { get; set; } = string.Empty;
+    public string ProtocolVersion { get; set; } = string.Empty;
+    public RequestHeader Headers { get; set; } = new RequestHeader();
+}
+
+public record RequestHeader
+{
+    public string UserAgent { get; set; } = string.Empty;
+    public int Port { get; set; } = 0;
+    public IPAddress Address { get; set; } = IPAddress.Any;
+}
+
