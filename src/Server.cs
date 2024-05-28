538ca3699f7a07eda1e1e2a3d28eb9f2def58338
Elkhunder <80496269+Elkhunder@users.noreply.github.com>
2024-05-28 10:52:13 -0400

    fixed issues

--- src/Server.cs
+++ src/Server.cs
@@ -196,14 +196,22 @@ public record HttpResponse(string Status, string? ContentType = null, int Conten
 
     public override string ToString()
     {
-        var sb = new StringBuilder();
-        sb.Append($"{Status}").Append("\r\n");
-        if (Body == null) return sb.ToString();
-        sb.AppendLine($"Content-Type: {ContentType}\r");
-        sb.AppendLine($"Content-Length: {ContentLength}\r");
-        sb.AppendLine();
-        sb.Append(Body);
-        return sb.ToString();
+        if (Body == null)
+        {
+            return $"Status\r\n";
+        }
+        // var sb = new StringBuilder();
+        // sb.Append($"{Status}").Append("\r\n");
+        // if (Body == null) return sb.ToString();
+        // sb.AppendLine($"Content-Type: {ContentType}\r");
+        // sb.AppendLine($"Content-Length: {ContentLength}\r");
+        // sb.AppendLine();
+        // sb.Append(Body);
+        return $"{Status}\r\n" + // Status line (includes protocol version and status code)"
+               $"Content-Type: {ContentType}\r\n" + // Content-Type header
+               $"Content-Length: {ContentLength}\r\n" + // Content-Length header
+               "\r\n" + // Empty line indicates the end of headers
+               Body; // Response body content
     }
 }
 public record Request
