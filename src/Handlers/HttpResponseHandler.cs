6d035c3387b3d4f6da7262af426127c4c0a6b202
Elkhunder <jpsissom@icloud.com>
2024-06-03 10:31:57 -0400

    Removed CLRF

--- src/Handlers/HttpResponseHandler.cs
+++ src/Handlers/HttpResponseHandler.cs
@@ -12,17 +12,8 @@ public abstract class HttpResponseHandler()
             var buffer = Encoding.ASCII.GetBytes(httpResponse.ToString());
             Console.WriteLine($"###{nameof(HttpResponseHandler)}" +
                               $"#####\r\n" +
-                              $"#####{nameof(HttpResponse)}: {httpResponse}" +
-                              $"#####\r\n" +
-                              $"#####{nameof(Encoding.UTF8)}-{nameof(Encoding.UTF8.GetBytes)}: {buffer}" +
-                              $"#####\r\n" +
-                              $"#####{nameof(ArraySegment<byte>)}: {new ArraySegment<byte>(buffer).ToString()}");
-            var sb = new StringBuilder();
-            foreach (var b in  buffer)
-            {
-                sb.Append($"{b}\r\n");
-            }
-            Console.WriteLine(sb);
+                              $"#####{nameof(HttpResponse)}: {httpResponse}");
+            Console.WriteLine();
             var bytesSent = await socket.SendAsync(buffer, SocketFlags.None);
             Console.WriteLine($"Bytes Sent: {bytesSent}");
         }
